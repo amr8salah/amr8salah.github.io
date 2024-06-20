915d9668131ca19a8fdc3d856dac7c10e62b16ae
Amr Salah <amr0103salah@gmail.com>
2024-06-20 17:36:15 +0300

    Added files

---
+++ main.js
@@ -0,0 +1,28 @@
+function goToAbout() {
+    document.getElementById("about-header").scrollIntoView({behavior: 'smooth'});
+}
+function goToExperience() {
+    document.getElementById("experience-header").scrollIntoView({behavior: 'smooth'});
+}
+function goToProjects() {
+    document.getElementById("projects-header").scrollIntoView({behavior: 'smooth'});
+}
+function goToContact() {
+    document.getElementById("contact-header").scrollIntoView({behavior: 'smooth'});
+}
+function openCV(){
+    window.open("./assets/CV.pdf");
+}
+function openLinkedin(){
+    window.open("https://www.linkedin.com/in/amr8salah/");
+}
+function openGithub(){
+    window.open("https://github.com/amr8salah");
+}
+function toggleMiniMenu(){
+    if(document.getElementById("mini-menu").style.display==="flex"){
+        document.getElementById("mini-menu").style.display="none";
+    } else{
+        document.getElementById("mini-menu").style.display="flex";
+    }
+}
